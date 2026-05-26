67b7f262aa6f6f940640e471f219f9f0454e8b65
thijs-vanweezel <thijsvanweezel@gmail.com>
2026-05-26 17:24:02 +0200

    require embedded inputs at each agentic step

--- rollout.py
+++ rollout.py
@@ -54,42 +54,44 @@ async def execute(model:transformers.modeling_utils.PreTrainedModel, tokenizer, 
     if image is not None:
         messages[1]["content"] += [{"type": "image", "image": image}]
     messages[1]["content"] += [{"type": "text", "text": text}, {"type": "text", "text": "<|state>"+"<state|>"}]
-    # Tokenize input
-    inputs = tokenizer.apply_chat_template(
-        messages,
-        add_generation_prompt=True,
-        tokenize=True,
-        return_tensors="pt",
-        return_dict=True,
-        enable_thinking=True,
-        tools=tools
-    ).to(model.device)
-    # Get embeddings
-    inputs_embeds = model.get_input_embeddings()(inputs["input_ids"])
-    if ple_submodel:=next((m for m in model.modules() if hasattr(m, "get_per_layer_inputs")), False):
-        per_layer_inputs = ple_submodel.get_per_layer_inputs(inputs["input_ids"], None)
 
     for _ in range(max_steps):
+        # Tokenize input
+        inputs = tokenizer.apply_chat_template(
+            messages,
+            add_generation_prompt=True,
+            tokenize=True,
+            return_tensors="pt",
+            return_dict=True,
+            enable_thinking=True,
+            tools=tools
+        ).to(model.device)
+
+        # Most models are decoder-only, so we can directly pass (modified) embeddings
+        inputs_embeds = model.get_input_embeddings()(inputs["input_ids"])
+        # Some models have per-layer embeddings
+        if ple_submodel:=next((m for m in model.modules() if hasattr(m, "get_per_layer_inputs")), False):
+            per_layer_inputs = ple_submodel.get_per_layer_inputs(inputs["input_ids"], None)
+
         # Determine location in embedding for the state representation
         state_token_id = tokenizer.encode("<|state>")[0]
         state_mask = (inputs["input_ids"] == torch.tensor(state_token_id).to(model.device)).reshape(-1)
         # Format current state into prompt
         state_features = state_encoder(env.state)
         inputs_embeds[state_mask:state_mask+len(state_features)] = state_features
-
-        # Most models are decoder-only, so we can directly pass (modified) embeddings
+        
+        # Prepare inputs for generation
         kwargs = {
             "attention_mask": inputs["attention_mask"],
             "mm_token_type_ids": inputs["mm_token_type_ids"],
             "use_cache": True,
             "logits_to_keep": 1,
             "output_logits": True,
-            "return_dict_in_generate": True
+            "return_dict_in_generate": True,
+            "inputs_embeds": inputs_embeds
         }
-        kwargs["inputs_embeds"] = inputs_embeds
-        if ple_submodel: 
+        if ple_submodel:
             kwargs["per_layer_inputs"] = per_layer_inputs
-        
         # Generate next action
         outputs = model.generate(
             **kwargs
