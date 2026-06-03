e06d559b9d879aa3cae17fa320573db5e3aec9cf
thijs-vanweezel <thijsvanweezel@gmail.com>
2026-06-03 21:34:52 +0200

    forfeit state token

--- setup.py
+++ setup.py
@@ -44,32 +44,6 @@ def fetch_model(model_id="google/gemma-4-E2B-it") -> tuple[AutoModelForImageText
         offload_buffers=True,
     )
     processor = AutoProcessor.from_pretrained(model_id)
-    # Add new special tokens for state representation
-    init_new_tokens(["<|state>", "<state|>"], ["environment", "observation", "start", "end"], torch.tensor([[.3,.3,.4,0.],[.3,.3,0.,.4]]), model, processor.tokenizer)
     # Find tool call tokens
     tool_tokens = find_tool_call_tokens(processor.tokenizer)
     return model, processor, tool_tokens
-
-def init_new_tokens(new_tokens, like_tokens, weights, model, tokenizer):
-    """
-    Args:
-        new_tokens: list of new token strings to add to the tokenizer
-        like_tokens: list of existing token strings to base the new token embeddings on
-        weights: tensor of shape (len(new_tokens), len(like_tokens)) specifying the weights for combining the like_token embeddings to create the new token embeddings
-    Returns:
-        list of new token ids corresponding to the new_tokens
-    """
-    # Find embeddings of related tokens
-    token_ids = tokenizer.encode(like_tokens)
-    embed_layer = model.get_input_embeddings()
-    embeds = embed_layer(torch.tensor(token_ids, device=embed_layer.weight.device)).squeeze(1)
-    # Merge them into new similar tokens
-    assert weights.shape == (len(new_tokens), len(like_tokens))
-    assert torch.allclose(weights.sum(axis=1), torch.ones(len(new_tokens)))
-    embeds = (embeds * weights.to(embeds.device, embeds.dtype).unsqueeze(-1)).sum(axis=1)
-    # Insert into tokenizer
-    tokenizer.add_special_tokens({"additional_special_tokens": new_tokens})
-    model.resize_token_embeddings(len(tokenizer), mean_resizing=False)
-    with torch.no_grad():
-        embed_layer.weight.data[-len(new_tokens):] = embeds
-    return tokenizer.encode(new_tokens)
